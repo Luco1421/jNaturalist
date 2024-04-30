8bbfaa27c45d3fd3ad2f812d82ac3bb14600f33b
luco1421 <byluco1421@gmail.com>
2024-04-30 03:06:33 -0600

    sdasad

--- Front-End/js/carrusel.js
+++ Front-End/js/carrusel.js
@@ -1,16 +1,21 @@
-document.querySelectorAll('.card').forEach(i => i.addEventListener('load', () => {
-    info = obtenerDatos([i]);
-    i.querySelector('.image').querySelector('img').src = info[url];
-    }
-));
-
-document.querySelectorAll('.card').forEach(i => i.addEventListener('click', () => {
-    sessionStorage.setItem('taxon', [i]);
+document.querySelectorAll('.card').forEach(i => i.addEventListener('click', (e) => {
+    sessionStorage.setItem('taxon', i.classList[1]);
     window.location.href = 'info.html';
     }
 ));
 
-let p = insertar3(['getrandomoccurrences',12]);
+insertar3(['getrandomoccurrences',12]).then(function(data) {
+    console.log(data);
+    let i = 0;
+    document.querySelectorAll('.card').forEach(
+        async function(j) {
+            j.classList.add(`${data[i][0]}`);
+            j.querySelector('.love').querySelector('span').innerText = data[i][2];
+            url = await obtener(`select url from image where id_image = ${data[i++][12]}`);
+            j.querySelector('.image').querySelector('img').src = url;        
+        }
+    );
+});
 
 function insertar(query) {
     return new Promise((resolve, reject) => {
